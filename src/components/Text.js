a858e56902e4144809d12ebe0a85738fcee27c63
Dharini S <dharinisrinivasan.94@gmail.com>
2023-10-03 14:21:46 +0530

    Add files via upload

---
+++ src/components/Text.js
@@ -0,0 +1,19 @@
+// Text Component
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Text = ({label,onUpdate,value}) => {
+  return (
+    <div className="text-box">
+      <span className="label">{label}</span>
+      <textarea onChange={evt=> onUpdate(evt.target.value.replace(/(?:\r)/g, "\n\n"))}
+      value={value}/>
+    </div>
+  )
+};
+Text.prototypes={
+  onUpdate:PropTypes.func,
+  label:PropTypes.string.isRequired,
+  value:PropTypes.string
+}
+export default Text;
